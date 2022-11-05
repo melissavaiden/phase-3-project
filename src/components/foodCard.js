2d8976af841a2176cab4164d2a2faa5c773df43b
Melissa Vaiden <melissavaiden@gmail.com>
2022-11-05 14:41:30 -0400

    Food Card Formatting

--- src/components/foodCard.js
+++ src/components/foodCard.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function FoodCard() {
  return (
-    <img src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg"></img>
+    <div>
+        <img class='food_url' src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg"></img>
+        <h2>Food Name</h2>
+        <h3>Price</h3>
+        <p>Food Description</p>
+    </div>
  )
 }
 
