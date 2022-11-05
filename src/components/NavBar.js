48175f30b32b28f7c5074636828c7b7c4b7b0a92
Melissa Vaiden <melissavaiden@gmail.com>
2022-11-05 19:51:40 -0400

    Added buttons for food categories to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function NavBar() {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:9292/categories')
+    .then((r) => r.json())
+    .then((cat) => setCategories(cat))
+    },[])
+
+    console.log(categories)
+
+    let categoryNavButtons = categories.map((cat) => {
+       return <button>{cat}</button>
+    })
+
     return (
         <div>
-    
+            {categoryNavButtons}
         </div>
     )
 
