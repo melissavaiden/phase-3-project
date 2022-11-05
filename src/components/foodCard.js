d7ea90141712fb9412bf5dd819fc9d35639f6718
Melissa Vaiden <melissavaiden@gmail.com>
2022-11-05 14:29:37 -0400

    Home and Food Card Component Created

---
+++ src/components/foodCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function FoodCard() {
+ return (
+    <img src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg"></img>
+ )
+}
+
+export default FoodCard;
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FoodCard from "./foodCard";
+
+function Home() {
+    return (
+    <div>   
+         <h1>Chef's Kitchen</h1>
+         <FoodCard />
+    </div>
+)}
+
+export default Home;
